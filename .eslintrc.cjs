1f573de7818a5ba9413c4d45604c890d3502f837
doctorbilardo <g.derbes@paisanos.io>
2024-04-20 12:39:46 -0300

    chore: new rules eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -34,5 +34,15 @@ module.exports = {
             },
         ],
         quotes: ["error", "double"],
+        "padding-line-between-statements": [
+            "warn",
+            { blankLine: "always", prev: "*", next: ["return", "export"] },
+            { blankLine: "always", prev: ["const", "let", "var"], next: "*" },
+            {
+                blankLine: "any",
+                prev: ["const", "let", "var"],
+                next: ["const", "let", "var"],
+            },
+        ],
     },
 };
